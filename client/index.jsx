3ba267eb40a0140dec9e0153b9c0cfff7758bf39
Emanuel Ferraz <kamefrede@gmail.com>
2022-06-11 19:46:22 +0100

    Refactor client to use functional components and hooks

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function getDistance(lat1, lon1, lat2, lon2) {
@@ -18,174 +18,147 @@ function getDistance(lat1, lon1, lat2, lon2) {
   return d;
 }
 
-class Grass extends React.Component {
-  generateLink() {
-    let grass = this.props.grass;
-    let name = grass.name;
-    let plus = grass.plus_code ? "+" + grass.plus_code.compound_code : "";
+const Grass = ({coords, grass}) => {
+  const { name, plus_code, geometry: { location } } = grass;
+
+  const generateLink = () =>  {
+    const plus = plus_code ? "+" + plus_code.compound_code : "";
     return `https://www.google.com/maps/search/${name}${plus}`;
   }
-  computeDistance() {
-    let srcLoc = this.props.coords;
-    let destLoc = this.props.grass.geometry.location;
+
+  const computeDistance = () => {
+    const srcLoc = coords;
+    const destLoc = location;
     if (!srcLoc || !destLoc) return 0;
-    let lat1 = srcLoc.latitude;
-    let lon1 = srcLoc.longitude;
-    let lat2 = destLoc.lat;
-    let lon2 = destLoc.lng;
+    const {longitude: lon1, latitude: lat1} = srcLoc;
+    const {longitude: lat2, latitude: lon2} = srcLoc;
+
     return getDistance(lat1, lon1, lat2, lon2);
   }
-  render() {
-    let { icon, name } = this.props.grass;
-    return (
-      <a className="p-2 bg-green-500 hover:bg-green-600 rounded-lg w-full"
-         tabIndex="1"
-         href={this.generateLink()}
-         target="_blank">
-        <div className="flex flex-row">
-          <span className="text-left grow">{name}</span>
-          <span className="text-right basis-1/4">({this.computeDistance().toFixed(0)}m)</span>
-        </div>
-      </a>
-    );
-  }
+
+  return (
+    <a className="p-2 bg-green-500 hover:bg-green-600 rounded-lg w-full"
+       tabIndex="1"
+       href={generateLink()}
+       target="_blank">
+      <div className="flex flex-row">
+        <span className="text-left grow">{name}</span>
+        <span className="text-right basis-1/4">({computeDistance().toFixed(0)}m)</span>
+      </div>
+    </a>
+  );
 }
 
-class GrassList extends React.Component {
-  render() {
-    if (this.props.grass.parks.length === 0) {
-      return (
-        <div>
-          <h2 className="p-2 text-2xl">
-            We couldn't find any grass nearby, sorry about that.
-          </h2>
-        </div>
-      );
-    }
+const GrassList = ({coords, grass: { parks } }) => {
+  if (parks?.length === 0) {
     return (
-      <div className="my-2">
-        <h2 className="pb-2 text-2xl">Grass near you</h2>
-        <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-1">
-          {this.props.grass.parks.map((grass, i) =>
-            <Grass key={i}
-                   coords={this.props.coords}
-                   grass={grass} />)}
-        </div>
+      <div>
+        <h2 className="p-2 text-2xl">
+          We couldn't find any grass nearby, sorry about that.
+        </h2>
       </div>
     );
   }
+  return (
+    <div className="my-2">
+      <h2 className="pb-2 text-2xl">Grass near you</h2>
+      <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-1">
+        {parks?.map((grass, i) =>
+          <Grass key={i}
+                 coords={coords}
+                 grass={grass} />)}
+      </div>
+    </div>
+  );
 }
 
-class Root extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      coords: null,
-      error: null,
-      grass: null,
-      loading: false,
-    };
-  }
+const Root = () => {
+  const [coords, setCoords] = useState({});
+  const [error, setError] = useState("");
+  const [grass, setGrass] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  render() {
-    let locateGrass = () => {
-      if (navigator.geolocation) {
-        this.setState({
-          ...this.state,
-          loading: true,
-        });
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            let { longitude, latitude } = position.coords;
-            this.setState({
-              ...this.state,
-              coords: position.coords,
+  const locateGrass = () => {
+    if (navigator.geolocation) {
+      setLoading(true);
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { longitude, latitude } = position.coords;
+
+          setCoords(position.coords)
+
+          fetch(`grass.json?longitude=${longitude}&latitude=${latitude}`)
+            .then(resp => {
+              if (resp.status !== 200) {
+                return resp.text().then(text => {
+                  throw Error(text)
+                });
+              } else {
+                return resp.json();
+              }
+            })
+            .then(grass => {
+              setLoading(false);
+              setError(null);
+              setGrass(grass);
+            })
+            .catch(err => {
+              setLoading(false);
+              setError(err.message);
             });
-            fetch(`grass.json?longitude=${longitude}&latitude=${latitude}`)
-              .then(resp => {
-                if (resp.status !== 200) {
-                  return resp.text().then(text => {
-                    throw Error(text)
-                  });
-                } else {
-                  return resp.json();
-                }
-              })
-              .then(grass => {
-                this.setState({
-                  ...this.state,
-                  loading: false,
-                  error: null,
-                  grass: grass
-                });
-              })
-              .catch(err => {
-                this.setState({
-                  ...this.state,
-                  loading: false,
-                  error: err.message,
-                });
-              });
-          },
-          (err) => {
-            if (err.code === 1) { // PERMISSION_DENIED
-              this.setState({
-                ...this.state,
-                loading: false,
-                error: true,
-              });
-            } else {
-              this.setState({
-                ...this.state,
-                loading: false,
-                error: err.message,
-              });
-            }
+        },
+        (err) => {
+          setLoading(false);
+          if (err.code === 1) { // PERMISSION_DENIED
+            setError(true);
+          } else {
+            setError(err.message);
           }
-        );
-      } else {
-        this.setState({
-          ...this.state,
-          error: "Your browser doesn't support geolocation."
-        });
-      }
-    };
-    return (
-      <section className="p-4 container mx-auto text-center">
-        <h1 className="p-2 text-3xl">Touch grass</h1>
-        {this.state.grass && <GrassList coords={this.state.coords} grass={this.state.grass} />}
-        {this.state.error &&
-         (this.state.error === true ?
-          (<div className="my-2 lg:w-1/2 md:w-2/3 mx-auto text-left">
-             <p className="text-lg text-red-600">
-               Grass location failed: You denied geolocation
-             </p>
-             <p>
-               This may come as a huge shock to you, but I do, in fact,
-               need to know your location in order to tell you where <b>nearby</b> grass is.
-             </p>
-             <p>
-               I don't store your location, but if you don't trust me,
-               you can take a look at
-               the <a href="https://github.com/eutro/touch_grass"
-                      className="text-blue-600 hover:text-blue-800">
-                     source code
-                   </a> and/or
-               run this yourself.
-             </p>
-             <p>
-               You might have to refresh the page to try again.
-             </p>
-           </div>) :
-          (<p className="p-2 text-red-600">Grass location failed: {this.state.error}</p>))}
-        <button className={(this.state.loading ? "cursor-wait animate-pulse bg-blue-700" : "bg-blue-700 hover:bg-blue-800")
-                           + " p-2 text-white rounded-full w-full"}
-                onClick={this.state.loading ? null : locateGrass}>
-            {this.state.loading ? "Locating nearby grass..." : "Locate nearby grass"}
-        </button>
-      </section>
-    );
-  }
+        }
+      );
+    } else {
+      setError("Your browser doesn't support geolocation.");
+    }
+  };
+
+
+  return (
+    <section className="p-4 container mx-auto text-center">
+      <h1 className="p-2 text-3xl">Touch grass</h1>
+      {grass && <GrassList coords={coords} grass={grass} />}
+      {error &&
+       (error === true ?
+        (<div className="my-2 lg:w-1/2 md:w-2/3 mx-auto text-left">
+           <p className="text-lg text-red-600">
+             Grass location failed: You denied geolocation
+           </p>
+           <p>
+             This may come as a huge shock to you, but I do, in fact,
+             need to know your location in order to tell you where <b>nearby</b> grass is.
+           </p>
+           <p>
+             I don't store your location, but if you don't trust me,
+             you can take a look at
+             the <a href="https://github.com/eutro/touch_grass"
+                    className="text-blue-600 hover:text-blue-800">
+                   source code
+                 </a> and/or
+             run this yourself.
+           </p>
+           <p>
+             You might have to refresh the page to try again.
+           </p>
+         </div>) :
+        (<p className="p-2 text-red-600">Grass location failed: {error}</p>))}
+      <button className={(loading ? "cursor-wait animate-pulse bg-blue-700" : "bg-blue-700 hover:bg-blue-800")
+                         + " p-2 text-white rounded-full w-full"}
+              onClick={loading ? null : locateGrass}>
+          {loading ? "Locating nearby grass..." : "Locate nearby grass"}
+      </button>
+    </section>
+  );
+
 }
 
 window.addEventListener("load", function() {
