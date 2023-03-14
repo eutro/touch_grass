67639935ec7796e174b3358698475e4e8657b41e
eutro <git@eutro.dev>
2023-03-14 22:09:11 +0000

    Add more types of grass

--- server/index.js
+++ server/index.js
@@ -64,16 +64,21 @@ function fileLoader(path, mime) {
 }
 
 async function grassLoader(req, res, next) {
-  const { longitude, latitude } = req.body;
+  const { longitude, latitude, what } = req.body;
+  let parks = undefined;
   if (!longitude
       || !latitude
       || !isFinite(longitude)
-      || !isFinite(latitude)) {
+      || !isFinite(latitude)
+      || !(parks = await getNearby(longitude, latitude, what))) {
     res.writeHead(400, {"Content-Type": "text/plain"});
-    res.write("400 Bad Request");
+    if (parks === undefined) {
+      res.write("400 Bad Request");
+    } else {
+      res.write("Unsupported type");
+    }
     res.end();
   } else {
-    const parks = await getParks(longitude, latitude);
     if (parks.status === 200) {
       if (parks.data.status === "ZERO_RESULTS" || parks.data.status === "OK") {
         res.writeHead(200, {"Content-Type": "text/json"});
@@ -91,7 +96,43 @@ async function grassLoader(req, res, next) {
   }
 }
 
-async function getParks(longitude, latitude) {
-  let url = `${placesRoot}/nearbysearch/json?location=${latitude}%2C${longitude}&rankby=distance&type=park&key=${placesKey}`;
+const placeTypes = new Set(
+  `accounting airport amusement_park aquarium art_gallery atm bakery
+  bank bar beauty_salon bicycle_store book_store bowling_alley
+  bus_station cafe campground car_dealer car_rental car_repair
+  car_wash casino cemetery church city_hall clothing_store
+  convenience_store courthouse dentist department_store doctor
+  drugstore electrician electronics_store embassy fire_station florist
+  funeral_home furniture_store gas_station gym hair_care
+  hardware_store hindu_temple home_goods_store hospital
+  insurance_agency jewelry_store laundry lawyer library
+  light_rail_station liquor_store local_government_office locksmith
+  lodging meal_delivery meal_takeaway mosque movie_rental
+  movie_theater moving_company museum night_club painter park parking
+  pet_store pharmacy physiotherapist plumber police post_office
+  primary_school real_estate_agency restaurant roofing_contractor
+  rv_park school secondary_school shoe_store shopping_mall spa stadium
+  storage store subway_station supermarket synagogue taxi_stand
+  tourist_attraction train_station transit_station travel_agency
+  university veterinary_care zoo`
+    .split(/\s+/));
+
+function unplural(what) {
+  if (what.endsWith("ies")) {
+    return what.substring(0, what.length - 3) + "y";
+  } else if (what.endsWith("s")) {
+    return what.substring(0, what.length - 1);
+  }
+  return what;
+}
+
+async function getNearby(longitude, latitude, what) {
+  let gmapType = "park";
+  what = unplural(what);
+  if (placeTypes.has(what)) gmapType = what;
+  else if (what === "vet") gmapType = "veterinary_care";
+  else return false;
+  let url = `${placesRoot}/nearbysearch/json?location=`
+      + `${latitude}%2C${longitude}&rankby=distance&type=${gmapType}&key=${placesKey}`;
   return await axios.get(url);
 }
